cb2a8c85aecbdec150759e5eff9a62d5b20e7614
ian akoto <iancecilakoto@gmail.com>
2020-03-25 07:11:57 -1200

    e learning application basic component setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {RouterModule} from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot([
+      { path: '', component: LoginComponent },
+      { path: 'dashboard/', component: DashboardComponent },
+    ])
+
   ],
   providers: [],
   bootstrap: [AppComponent]
