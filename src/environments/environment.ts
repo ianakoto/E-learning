0b08604788e3b7ce5014c80da9a46bee7298dcfc
ian akoto <iancecilakoto@gmail.com>
2020-04-06 07:49:24 -1200

    production changes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,14 +5,7 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: 'AIzaSyDfi-JM3VrCsojj4GDoPRVkRJJIlCW8uNY',
-    authDomain: 'happy-kids-e-learning.firebaseapp.com',
-    databaseURL: 'https://happy-kids-e-learning.firebaseio.com',
-    projectId: 'happy-kids-e-learning',
-    storageBucket: 'happy-kids-e-learning.appspot.com',
-    messagingSenderId: '994321312721',
-    appId: '1:994321312721:web:31723ae8d15c9691e24ef0',
-    measurementId: 'G-QZWCCS4R5B'
+
 }
 };
 
